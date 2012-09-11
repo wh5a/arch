4bdd4aa88d3edc0eb5eb97001840f91ae9a7a07a
Wei Hu <weihu@cs.virginia.edu>
2012-09-11 06:57:05 +0000

    Update xmonad patch

--- bin/publish-repo.sh
+++ bin/publish-repo.sh
@@ -1,1 +1,1 @@
-cd /srv/http; rm wei.db.tar.gz; repo-add wei.db.tar.gz *xz; rsync -avz --del /srv/http wh5a@power6.cs.virginia.edu:/home/wh5a/public_html
+cd /srv/http; rm wei.db.tar.gz; repo-add wei.db.tar.gz *xz; rsync -avz --del /srv/http wh5a@power3.cs.virginia.edu:/home/wh5a/public_html
